1ca0dee44736f6e83706b28cbc087508d9ebaa8f
mianfeng <1816670208@qq.com>
2026-06-18 23:39:43 +0800

    chore: configure Google API credentials

--- src/google-config.js
+++ src/google-config.js
@@ -1,5 +1,5 @@
 window.DAYCAM_GOOGLE_CONFIG = {
-  apiKey: "",
-  clientId: "",
-  appId: "",
+  apiKey: "AIzaSyB8zo_Tx3AE8gNm68UVJStnxLNz5YyAOwU",
+  clientId: "424443007004-2bthanni5c99v7kkolt0cerpqcev21d9.apps.googleusercontent.com",
+  appId: "424443007004",
 };
